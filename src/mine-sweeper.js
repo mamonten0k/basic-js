0b58a7e06e323b94fe4450d80f1325f89d3a46a0
mamonten0k <asdnbvb123@gmail.com>
2022-05-02 11:08:19 +0300

    feat: finish mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let mineField = Array(matrix.length)
+    .fill()
+    .map((subArr) => Array(matrix[0].length));
+  for (let i = 0; i < matrix.length; i++)
+    for (let j = 0; j < matrix[0].length; j++)
+      mineField[i][j] = calculateCell(matrix, i, j);
+  return mineField;
+}
+
+function calculateCell(matrix, i, j) {
+  let sum = 0;
+  if (i == 0 && j == 0) {
+    sum += matrix[i][j + 1];
+    sum += matrix[i + 1][j + 1];
+    sum += matrix[i + 1][j];
+  } else if (i == 0 && j == matrix[0].length - 1) {
+    sum += matrix[i][j - 1];
+    sum += matrix[i + 1][j - 1];
+    sum += matrix[i + 1][j];
+  } else if (i == matrix.length - 1 && j == 0) {
+    sum += matrix[i][j + 1];
+    sum += matrix[i - 1][j + 1];
+    sum += matrix[i - 1][j];
+  } else if (i == matrix.length - 1 && j == matrix[0].length - 1) {
+    sum += matrix[i][j - 1];
+    sum += matrix[i - 1][j - 1];
+    sum += matrix[i - 1][j];
+  } else if (i == 0) {
+    sum += matrix[i][j - 1];
+    sum += matrix[i][j + 1];
+    sum += matrix[i + 1][j - 1];
+    sum += matrix[i + 1][j + 1];
+    sum += matrix[i + 1][j];
+  } else if (i == matrix.length - 1) {
+    sum += matrix[i][j - 1];
+    sum += matrix[i][j + 1];
+    sum += matrix[i - 1][j - 1];
+    sum += matrix[i - 1][j + 1];
+    sum += matrix[i - 1][j];
+  } else if (j == 0) {
+    sum += matrix[i][j + 1];
+    sum += matrix[i + 1][j];
+    sum += matrix[i - 1][j];
+    sum += matrix[i + 1][j + 1];
+    sum += matrix[i - 1][j + 1];
+  } else if (j == matrix[0].length - 1) {
+    sum += matrix[i][j - 1];
+    sum += matrix[i + 1][j];
+    sum += matrix[i - 1][j];
+    sum += matrix[i + 1][j - 1];
+    sum += matrix[i - 1][j - 1];
+  } else {
+    sum += matrix[i][j - 1];
+    sum += matrix[i][j + 1];
+    sum += matrix[i + 1][j];
+    sum += matrix[i - 1][j];
+    sum += matrix[i + 1][j + 1];
+    sum += matrix[i + 1][j - 1];
+    sum += matrix[i - 1][j + 1];
+    sum += matrix[i - 1][j - 1];
+  }
+  return sum;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
